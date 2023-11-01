fd0804920d6238bd0e11967dcd651cfc7989eb1a
MatildaGomezLindblom <matildagoli@outlook.com>
2023-11-01 11:53:15 +0100

    navbar styled and fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import ModeToggler from "./ModeToggler";
+import "../styles/Navbar.scss"; // Create a new stylesheet for Navbar styles
+import logo from "../assets/logo.png";
 
-function Navbar({toggleTheme}) {
+import { motion} from "framer-motion";
+
+function Navbar({ toggleTheme }) {
   const navigate = useNavigate();
 
   function scrollToSection(id) {
@@ -11,20 +15,35 @@ function Navbar({toggleTheme}) {
   }
 
   return (
-    <nav>
-      <ul>
-      <li>
-          <ModeToggler toggleTheme={toggleTheme}/>
-        </li>
+    <nav className="navbar-container">
+        <a
+          className="hero-link"
+          href="/#hero"
+          onClick={(e) => {
+            e.preventDefault();
+            scrollToSection("hero");
+          }}
+        >
+          <motion.div whileHover={{ scale: [null, 1.25, 1.25], rotate: -15 }}>
+          <img
+            src={logo}
+            width="40"
+            height="40"
+            className="d-inline-block align-top"
+            alt="React Bootstrap logo"
+          />
+          </motion.div>
+        </a>
+      <ul className="navbar-list">
         <li>
           <a
-            href="/#hero"
+            href="/#about"
             onClick={(e) => {
               e.preventDefault();
-              scrollToSection("hero");
+              scrollToSection("about");
             }}
           >
-            Hero
+            OM MIG
           </a>
         </li>
         <li>
@@ -35,8 +54,16 @@ function Navbar({toggleTheme}) {
               scrollToSection("about");
             }}
           >
-            About
+            PROJEKT
           </a>
+        </li>
+        <li>
+          <a href="./CV.pdf" download="CV.pdf">
+            CV
+          </a>
+        </li>
+        <li>
+          <ModeToggler toggleTheme={toggleTheme} />
         </li>
       </ul>
     </nav>
