68d55f8de551111986314ce18f136a1d8bf6c50b
MatildaGomezLindblom <matildagoli@outlook.com>
2024-08-22 15:54:31 +0200

    Refactoring

--- src/components/projects/ProjectsSection.js
+++ src/components/projects/ProjectsSection.js
@@ -4,14 +4,8 @@ import { motion } from "framer-motion";
 import "../../styles/ProjectsSection.scss";
 
 function ProjectsSection() {
-  const [isTransitioning, setIsTransitioning] = useState(false);
   const [transitioningProject, setTransitioningProject] = useState(null);
   const navigate = useNavigate();
-  const [animationProps, setAnimationProps] = useState({
-    scale: 1,
-    x: 0,
-    y: 0,
-  });
 
   const projects = [
     { id: "project-1", page: "/Hotspot", tags: "React Native • Användarhantering • UX • Firebase", title: "HOTSPOT - Appen för att spara och dela sina favoritplatser med vänner."},
@@ -20,7 +14,6 @@ function ProjectsSection() {
     { id: "project-4", page: "/Hotspot", tags: "", title: ""},
   ];
 
-  // Function to handle the image click and start the transition
   const handleImageClick = (project) => {
     // Get properties of element
     const element = document.querySelector(`#${project.id}`);
@@ -34,19 +27,17 @@ function ProjectsSection() {
 
     // Calculate the scale factor to cover the entire screen width
     const scaleFactor = screenWidth / elementWidth;
-    console.log(scaleFactor);
 
-    // Calculate translation to center the element
+    //calculate translation to center the element
     const scaledWidth = elementWidth * scaleFactor;
     const scaledHeight = elementHeight * scaleFactor;
 
-    // Calculate translation to center the element
     const translateX =
       (screenWidth - scaledWidth) / 2 - elementRect.left + elementWidth / 2;
     const translateY =
       (screenHeight - scaledHeight) / 2 - elementRect.top + elementHeight / 2;
 
-    // Set the transitioning project and its animation properties
+    //set the transitioning project and its animation properties
     setTransitioningProject({
       id: project.id,
       scale: scaleFactor,
@@ -55,10 +46,9 @@ function ProjectsSection() {
       zIndex: 10000,
     });
 
-    // Apply the calculated scale factor
     setTimeout(() => {
       navigate(project.page);
-    }, 800); // Adjust the timeout to match the animation duration
+    }, 800); 
   };
 
   return (
@@ -97,72 +87,6 @@ function ProjectsSection() {
         )}
       </motion.div>
           ))}
- {/*        
-
-
-        <motion.div
-          className="project project-2"
-          initial={{ scale: 1, x: 0, y: 0, zIndex: 10 }}
-          animate={
-            isTransitioning
-              ? animationProps
-              : { scale: 1, x: 0, y: 0, zIndex: 10 }
-          }
-          transition={{ duration: 0.8, type: "easeIn" }}
-          onClick={() => handleImageClick("/Hotspot")}
-        >
-          {!isTransitioning && (
-            <div className="overlay">
-              <p>React Native • Användarhantering • UX • Firebase</p>
-              <h4>
-                HOTSPOT - Appen för att spara och dela sina favoritplatser med
-                vänner.
-              </h4>
-            </div>
-          )}
-        </motion.div>
-        <motion.div
-          className="project project-3"
-          initial={{ scale: 1, x: 0, y: 0, zIndex: 10 }}
-          animate={
-            isTransitioning
-              ? animationProps
-              : { scale: 1, x: 0, y: 0, zIndex: 10 }
-          }
-          transition={{ duration: 0.8, type: "easeIn" }}
-          onClick={() => handleImageClick("/Hotspot")}
-        >
-          {!isTransitioning && (
-            <div className="overlay">
-              <p>React Native • Användarhantering • UX • Firebase</p>
-              <h4>
-                HOTSPOT - Appen för att spara och dela sina favoritplatser med
-                vänner.
-              </h4>
-            </div>
-          )}
-        </motion.div>
-        <motion.div
-          className="project project-4"
-          initial={{ scale: 1, x: 0, y: 0, zIndex: 10 }}
-          animate={
-            isTransitioning
-              ? animationProps
-              : { scale: 1, x: 0, y: 0, zIndex: 10 }
-          }
-          transition={{ duration: 0.8, type: "easeIn" }}
-          onClick={() => handleImageClick("/Hotspot")}
-        >
-          {!isTransitioning && (
-            <div className="overlay">
-              <p>React Native • Användarhantering • UX • Firebase</p>
-              <h4>
-                HOTSPOT - Appen för att spara och dela sina favoritplatser med
-                vänner.
-              </h4>
-            </div>
-          )}
-        </motion.div> */}
       </div>
     </div>
   );
